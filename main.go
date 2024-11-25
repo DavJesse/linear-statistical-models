44d33b560d0f1b6c1f19e887ce5d0398807e3ade
davodhiambo <jesseomolo@yahoo.com>
2024-11-26 00:59:29 +0300

    feat: add parameters for catchning path parsed as argument on standand input.

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
+)
 
+func main() {
+	args := os.Args[1:]
+
+	if len(args) != 1 {
+		log.Fatal("Please provide a single file path to a valid text file as an argument.")
+	}
+	filePath := args[0]
 }
