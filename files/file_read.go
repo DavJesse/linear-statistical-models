9ecf8bd038f820a692b3422894d7d05b1c2e0064
davodhiambo <jesseomolo@yahoo.com>
2024-11-26 01:14:14 +0300

    feat: add ReadFile to Read content of file parsed as argument, storing content in slice of ints.

--- files/file_read.go
+++ files/file_read.go
@@ -1,7 +1,37 @@
 package files
 
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+)
+
 func ReadFile(path string) []int {
 	var result []int
+
+	// Open file for reading
+	// Hanndle errors during file opening here
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatalf("Error opening file: %v", err)
+	}
+	defer file.Close()
+
+	// Read file line by line
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		dataPoint := scanner.Text()
+
+		// Convert data read into integer
+		// Handle any errors encountered
+		num, err := strconv.Atoi(dataPoint)
+		if err != nil {
+			log.Fatalf("File may contain non-numeric data: %v", err)
+		}
+
+		result = append(result, num)
+	}
 
 	return result
 }
