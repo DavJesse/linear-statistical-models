315e424a2099216b34bd81f63c670e79edeb0b3b
davodhiambo <jesseomolo@yahoo.com>
2024-11-26 01:23:14 +0300

    chore check file validity.

--- main.go
+++ main.go
@@ -3,13 +3,25 @@ package main
 import (
 	"log"
 	"os"
+	"strings"
+
+	"linear-stats/files"
 )
 
 func main() {
 	args := os.Args[1:]
 
+	// Handle insufficient or incorrect number of arguments
 	if len(args) != 1 {
 		log.Fatal("Please provide a single file path to a valid text file as an argument.")
 	}
+
+	// Define file path from arguments
+	// Check file validity from its extenstion
 	filePath := args[0]
+	if !strings.HasPrefix(filePath, ".txt") {
+		log.Fatalf("Invalid file format: %v is not a text file", filePath)
+	}
+
+	data := files.ReadFile(filePath)
 }
