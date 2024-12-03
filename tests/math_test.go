fe6bb80a2d895b490585796e4876da86e1404425
davodhiambo <jesseomolo@yahoo.com>
2024-12-03 20:38:21 +0300

    testing: add TestExtractParams.

--- tests/math_test.go
+++ tests/math_test.go
@@ -2,8 +2,10 @@ package tests
 
 import (
 	"math"
+	"reflect"
 	"testing"
 
+	"linear-stats/files"
 	"linear-stats/maths"
 )
 
@@ -30,5 +32,16 @@ func TestPearsonCoefficient(t *testing.T) {
 	if difference > tolerance {
 		t.Errorf("Expected: %f, Got: %f", expected, got)
 		t.Error("TestVariance Failed")
+	}
+}
+
+func TestExtractParams(t *testing.T) {
+	subject := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	expected1, expected2 := []int{0, 2, 3, 4, 5, 6, 7, 8, 9}, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	got1, got2 := files.ExtractParams(subject)
+
+	if reflect.DeepEqual(got1, expected1) && reflect.DeepEqual(got2, expected2) {
+		t.Errorf("Expected: %v, %v, Got: %v, %v", expected1, expected2, got1, got2)
+		t.Error("TestExtractParams Failed")
 	}
 }
